5fcb850fd02c81499b264f20aa8bea84bbf480ec
Greg Major <greg.b.major@gmail.com>
2018-04-18 13:20:28 -0500

    Added existing link skip and overwrite capability

--- cmd/installDotfiles.go
+++ cmd/installDotfiles.go
@@ -15,11 +15,13 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
-    "os"
-    "path"
-    "path/filepath"
-    "regexp"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -35,15 +37,16 @@ var installDotfilesCmd = &cobra.Command{
     
     Run this command any time you have made changes to your dotfiles repo.`,
 	Run: func(cmd *cobra.Command, args []string) {
-        dotfilesDirectory := viper.GetString("dotfilesDirectory")
+		dotfilesDirectory := viper.GetString("dotfilesDirectory")
 
-        fmt.Printf("Dotfile directory: %s\n", dotfilesDirectory)
-        fmt.Printf("   Home directory: %s\n", viper.GetString("homeDirectory"))
+		fmt.Printf("Dotfile directory: %s\n", dotfilesDirectory)
+		fmt.Printf("   Home directory: %s\n", viper.GetString("homeDirectory"))
+		fmt.Println()
 
-        if err := filepath.Walk(dotfilesDirectory, visit); err != nil {
-            fmt.Println(err)
-            os.Exit(1)
-        }
+		if err := filepath.Walk(dotfilesDirectory, visit); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	},
 }
 
@@ -54,43 +57,94 @@ func init() {
 func visit(path string, f os.FileInfo, err error) error {
 	home := viper.GetString("homeDirectory")
 
-    if f.IsDir() && f.Name() == bundlesDir {
-        return filepath.SkipDir
-    }
+	if f.IsDir() && f.Name() == bundlesDir {
+		return filepath.SkipDir
+	}
 
-    if f.IsDir() {
-        matches, err := filepath.Glob(path + "/*.symlink")
-        if err == nil {
-            for _, match := range matches {
-                targetFile := home + "/" + getSymlinkTargetName(match)
-                if fileDoesNotExist(targetFile) {
-                    if err := os.Symlink(match, targetFile); err == nil {
-                        fmt.Printf("Symlinked %s => %s\n", match, targetFile)
-                    } else {
-                        fmt.Printf("ERROR symlinking %s: %s\n", targetFile, err)
-                    }
-                } else {
-                	fmt.Printf("Symlink for %s already exists. Skipping.\n", f.Name())
+	if f.IsDir() {
+		matches, err := filepath.Glob(path + "/*.symlink")
+		if err == nil {
+			for _, match := range matches {
+				targetFile := home + "/" + getSymlinkTargetName(match)
+				if fileDoesNotExist(targetFile) {
+					createSymlink(match, targetFile)
+				} else {
+					input := bufio.NewScanner(os.Stdin)
+
+					fmt.Printf("%s already exists. (S)kip or (O)verwrite?\n", targetFile)
+
+					InputLoop:
+					for input.Scan() {
+
+						answer := strings.ToLower(input.Text())
+
+						switch answer {
+						case "o":
+							fmt.Printf("Overwriting %s\n", f.Name())
+							if err := backupAndRemoveTarget(targetFile); err == nil {
+								createSymlink(match, targetFile)
+							}
+							break InputLoop
+						case "s":
+							fmt.Printf("Skipping %s\n", f.Name())
+							break InputLoop
+						default:
+							fmt.Println("Invalid response. (S)kip or (O)verwrite?")
+						}
+					}
 				}
-            }
-        } else {
-            return err
-        }
-    }
+			}
+		} else {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
 
-func getSymlinkTargetName(fileName string) string {
-    name := path.Base(fileName)
-    re := regexp.MustCompile("\\.symlink")
+func backupAndRemoveTarget(filename string) error {
+	backupExtension := ".backup"
+	backupFile := filename + backupExtension
 
-    return re.ReplaceAllString(name, "")
+	if _, err := os.Stat(backupFile); os.IsNotExist(err) {
+		fmt.Println("No existing backup")
+	} else {
+		if err := os.Remove(backupFile); err == nil {
+			fmt.Println("Existing backup removed")
+		} else {
+			fmt.Printf("ERROR removing backup %s: %s\n", backupFile, err)
+			return err
+		}
+	}
+
+	if err := os.Rename(filename, backupFile); err == nil {
+		fmt.Println("Backup created")
+	} else {
+		fmt.Printf("ERROR creating backup %s: %s\n", backupFile, err)
+		return err
+	}
+
+	return nil
+}
+
+func createSymlink(source string, target string) {
+	if err := os.Symlink(source, target); err == nil {
+		fmt.Printf("Symlinked %s => %s\n", source, target)
+	} else {
+		fmt.Printf("ERROR symlinking %s: %s\n", target, err)
+	}
 }
 
 // Attempt to get the FileInfo for the target and if there's an error then assume the file doesn't exist
 func fileDoesNotExist(targetFile string) bool {
-    _, err := os.Stat(targetFile)
+	_, err := os.Stat(targetFile)
 
-    return os.IsNotExist(err)
+	return os.IsNotExist(err)
+}
+
+func getSymlinkTargetName(fileName string) string {
+	name := path.Base(fileName)
+	re := regexp.MustCompile("\\.symlink")
+
+	return re.ReplaceAllString(name, "")
 }
